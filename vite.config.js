21a25e925c2d36cab5e2ae94ed2a8339dc418d81
Nawarat Patnantaporn <nawaratpatnantaporn@IScream-2.local>
2025-04-27 11:49:14 +0700

    first commit

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/NexusPay/', // เปลี่ยนเป็นชื่อ Repo ของคุณ
+  base: '/', 
 })
